83a7ffd214f572add11b6aac85265ace1e435f19
Christopher Dembia <cld72@cornell.edu>
2013-06-28 17:02:36 -0700

    Test the ability to compare value types.

--- tests/testValues.cpp
+++ tests/testValues.cpp
@@ -3,4 +3,13 @@
 TEST(TestValues, Length)
 {
     ASSERT_TRUE(false);
+
+    Meters myFirstLength();
+    Meters mySecondLength(1.53);
+
+    Centimeters myThirdLength(153.0);
+
+    // Test equality between mySecondLength and myThirdLength.
 }
+
+
